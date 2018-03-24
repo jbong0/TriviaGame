b615e2b08d267068c651bbefcbe8d4e7b8a97463
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-03-23 22:30:02 -0700

    Theme, Questions, and Answers set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,8 @@ var timeRemaining
 var correctGuesses 
 var wrongGuesses
 var noAnswer
+var audio 
+var soundEffects
 
 // questions&answers  
 var data = [
@@ -47,3 +49,19 @@ function setQuestion(){
 
 };
 
+
+
+// 1. Jeopardy! averages 25 million viewers per week.
+
+// 2. Alex Trebek’s real first name is George. Alexander is his middle name.
+
+// 3. One week's worth of episodes is shot in one day.
+//    To keep up the illusion of passing time, 
+//    Trebek changes his suit between shows.
+
+// 4. Jeopardy! holds a record 30 Daytime Emmy Awards.
+
+// 5. The original daytime version debuted on NBC on March 30, 1964, 
+//    and aired until January 3, 1975. A weekly nighttime syndicated edition aired 
+//    from September 1974 to September 1975, and a revival, The All-New Jeopardy!, 
+//    ran on NBC from October 1978 to March 1979.
