7ff9a9285faa22acf2fbdb20e2c458b5bbacad9b
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-03-23 22:17:57 -0700

    Psuedocode game, added varibles to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,49 @@
+var startButton
+var startTime
+var timeRemaining
+var correctGuesses 
+var wrongGuesses
+var noAnswer
+
+// questions&answers  
+var data = [
+    {
+    question:" ",
+    choices:["choiceOne", "choiceTwo", "choiceThree"],
+    correctAnswer: " "},
+     {
+    question:" ",
+    choices:["choiceOne", "choiceTwo", "choiceThree"],
+    correctAnswer: " "},
+     {
+    question:" ",
+    choices:["choiceOne", "choiceTwo", "choiceThree"],
+    correctAnswer: " "},
+     {
+    question:" ",
+    choices:["choiceOne", "choiceTwo", "choiceThree"],
+    correctAnswer: " "},
+     {
+    question:" ",
+    choices:["choiceOne", "choiceTwo", "choiceThree"],
+    correctAnswer: " "},
+]
+
+// correctAnswer images
+var images = [
+    "assets/images/ ",
+    "assets/images/ ",
+    "assets/images/ ",
+    "assets/images/ ",
+    "assets/images/ ",
+]
+
+
+
+
+// display questions&answers
+function setQuestion(){
+    var currentQuestion = data.question[0]
+
+};
+
