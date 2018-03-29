966340d2a0391192c0190c747cea17af73a23825
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-03-29 13:19:08 -0700

    Results div to display correct and incorrect answer/ Also added timeouts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,70 +20,84 @@ var quiz = [{
 },{
     question:"How many episodes of 'Jeopardy' are shot per day of filming? ",
     choices:[ "5 Episodes", "1 Episode","3 Episodes",  "2 Episodes"],
-    answerIndex:0,
+	answerIndex: 0,
 }]
 
 var userChoice
+var timer = $("#timer")
+var results = $("#results")
 var i = 0
 var intervalId 
 var right = 0
 var wrong = 0
-var rightDiv = $("#rightDiv")
-var wrongDiv = $("#wrongDiv")
-var countdown = 11
+var countdown = 10
+
 
 $(document).ready(function() {
 
     // change background and display timer on click of jumbotron
     $(".jumbotron").click(function(){
         $(this).css('background-image', 'url(assets/images/jeopardyfluid.png)');
-        $("#timer").html('<p>Time Remaining : '+ 10  + '</p>');
-        $(this).attr("class","fluid mt-5");
-        decrement(); run(); loadData(i);
+        timer.html('<p>Time Remaining : '+ 10  + '</p>');
+		$(this).attr("class","fluid mt-5");
+		loadData(i);
+		
      
 	});
 	
+	
+
 	$("#button1, #button2, #button3, #button4").click(function(){
 
 		userChoice = $(this).index();
 		stop();
 
 	 if (userChoice === quiz[i].answerIndex) 
-		 {
-			 alert("Correct!");
+		 {	
+		     winSound.play()
+			 setTimeout(function(){ alert("Correct!"); }, 1000)
 			 i++;
 			 right++;
-			 $("#question").empty();
-			 $("#choice1, #choice2, #choice3, #choice4").empty();
-			 loadData(i);
+			 $("#question").empty()
+			 $("#choice1, #choice2, #choice3, #choice4").empty()
+			 loadData(i)
+
 		 }
 	 
 	 else{
-			 alert("Wrong!");
+			wrongSound.play() 
+			setTimeout(function(){ alert("Wrong!"); }, 1000)
 			 i++;
 			 wrong++;
 			 $("#question").empty();
 			 $("#choice1, #choice2, #choice3, #choice4").empty();
-			 loadData(i);    		
+			 loadData(i);    	
 		 }
 	 });
 
 });
 
  function loadData(i){
-
+	
 	 $("#question").html(quiz[i].question)
 	 $("#choice1").html(quiz[i].choices[0])
 	 $("#choice2").html(quiz[i].choices[1])
 	 $("#choice3").html(quiz[i].choices[2])
 	 $("#choice4").html(quiz[i].choices[3])
-	 run();
+	 endGame(); run()
 	 
-	if(i===4){
-		alert("You got " + right + " correct, and " + wrong + " wrong!");
-		stop(); endGame();
+if(i===4){
+		setTimeout(function(){
+			$("#question").html("<h2> You got </h2>")
+			
+	   }, 1000)
 	}
  }
+
+ function endGame(){
+	$("#rightDiv").html("Correct Answers : " + right);
+	$("#wrongDiv").html("Wrong Answers : " + wrong);
+  }
 
 
 
@@ -98,30 +112,58 @@ function decrement(){
 
 	countdown -- ;
 	
-    $("#timer").html("Time Remaining : " + countdown);
+    timer.html("Time Remaining : " + countdown);
 
         if (countdown === 0){
-            alert("Time has run out!");
+			wrongSound.play() 
+            setTimeout(function(){ console.log("Time has run out!"); }, 1000);
 			wrong++;
 			i++;
 			$("#question").empty();
 		  	$("#choice1, #choice2, #choice3, #choice4").empty();
+			timer.html('Time Remaining : ' + "0" );
+			loadData(i); 
+			
 			stop();
-			$("#timer").html('Time Remaining : ' + countdown );
-			loadData(i);
     }
 }
 // clear timer
 function stop() {
     clearInterval(intervalId);
-    countdown=10;
+	countdown = 10;
+	endGame();
   }
 
-  function endGame(){
-	  rightDiv.html("Correct Answers :" + right);
-	  wrongDiv.html("Wrong Answers :" + wrong);
-	  if (countdown < 0){
-		  $("#timer").html('Time Remaining : 0 ');
-	  }
 
-  }
+  
+
+// Styling
+
+	// // Audio
+	// var isPlaying = false;
+	// function togglePlay() {
+	// 	if (isPlaying) {
+	// 	myAudio.pause()
+	// 		} else {
+	// 		myAudio.play();
+	// 		}
+	// 		};
+	// 		myAudio.onplaying = function() {
+	// 			isPlaying = true;
+	// 			$('#music').html("Music &#128266;")
+	// 			};
+	// 		myAudio.onpause = function() {
+	// 			isPlaying = false;
+	// 			$('#music').html("Music &#128263;")
+	// };
+
+	// mouse effects
+	$('.btn-block').mouseover(function(event) {
+	$(event.target).addClass('outlineElement');
+	})
+	$('.btn-block').mouseout(function(event) {
+	$(event.target).removeClass('outlineElement');
+	})
+
+	
+
